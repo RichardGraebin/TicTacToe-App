86d7a36d3da4ce680f01edc0c10f9a9ba33c8741
Richard Graebin <77891472+RichardGraebin@users.noreply.github.com>
2024-10-23 19:24:33 -0300

    implemented player name component and editting name feature

---
+++ src/components/Player.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditMode() {
+    setIsEditing((editing) => !editing); // best practice instead of just !isEditing
+  }
+
+  function handleChangue(e) {
+    setPlayerName(e.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && <input type="text" required value={playerName} onChange={handleChangue} />}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditMode}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
