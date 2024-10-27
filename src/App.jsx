627ab0d6774b4345c64ca65975216fba3ec47df2
Richard Graebin <77891472+RichardGraebin@users.noreply.github.com>
2024-10-27 10:21:07 -0300

    implemented log component

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,7 @@ function App() {
         </ol>
         <GameBoard onSelectSquare={handleSelectedSquare} turns={gameTurns} />
       </div>
-      <Log />
+      <Log turns={gameTurns} />
     </main>
   );
 }
